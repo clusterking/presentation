cae6558689ba3ff5f44f06521bb9387ac2def945
Kilian Lieret <kilian.adriano.lieret@desy.de>
2019-03-15 13:09:57 +0100

    plots

---
+++ code/plotting.py
@@ -0,0 +1,4 @@
+cp = ClusterPlot(df)
+cp.scatter(['CVL_bctaunutau', 'CSL_bctaunutau', 'CT_bctaunutau'])
+cp.scatter(['CVL_bctaunutau', 'CSL_bctaunutau'])
+cp.fill(['CVL_bctaunutau', 'CSL_bctaunutau'])
--- figures/hierarchy/source/convert.sh
+++ figures/hierarchy/source/convert.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-for f in *.svg; do; echo $f; inkscape $f --export-pdf ../${f%.*}.pdf; done
+for f in *.svg; do echo $f; inkscape $f --export-pdf ../${f%.*}.pdf; done
